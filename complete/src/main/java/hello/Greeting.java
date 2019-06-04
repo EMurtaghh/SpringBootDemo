e4384b6dcf27ed2ae30eb0f4f00ac11cc2e196ae
Aine <m.g.firefox@gmail.com>
2019-06-04 15:14:09 -0400

    updated text in questions

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -2,14 +2,14 @@ package hello;
 
 public class Greeting {
 
-    private long id;
+    private String id;
     private String content;
 
-    public long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
