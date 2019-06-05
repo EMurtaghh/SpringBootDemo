f077cabfe7a483afe36894b3059b1ffaa5b02c8e
Aine <m.g.firefox@gmail.com>
2019-06-05 10:12:22 -0400

    drop downs

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -6,6 +6,24 @@ public class Greeting {
     private String content;
     private String horse;
     private String ghost;
+    private String number;
+    private String thot;
+
+    public String getThot() {
+        return thot;
+    }
+
+    public void setThot(String thot) {
+        this.thot = thot;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 
     public String getId() {
         return id;
