89357d8da9c7f390dd884cbe3c02c7361fe3555c
Emma <emurtagh03@gmail.com>
2019-06-10 09:12:57 -0400

    added multi question checking and image task example

--- complete/src/main/resources/static/js/AnswerCheckControl.js
+++ complete/src/main/resources/static/js/AnswerCheckControl.js
@@ -1,5 +1,23 @@
 function generateFeedback(isChecked) {
-    if (isChecked == true) {
+    if (isChecked) {
+        return "Correct!";
+    } else {
+        return "Incorrect. So sad :(";
+    }
+}
+
+function generateFeedbackMulti(isCheckedQ1, isCheckedQ2, isCheckedQ3) {
+    var count = 0;
+    if (isCheckedQ1) {
+        count++;
+    }
+    if (isCheckedQ2) {
+        count++;
+    }
+    if (isCheckedQ3) {
+        count++
+    }
+    if (count == 3) {
         return "Correct!";
     } else {
         return "Incorrect. So sad :(";
