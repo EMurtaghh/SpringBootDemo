93cbf5b38cedd1782f4280a74b53f4fdce6c937d
Emma <emurtagh03@gmail.com>
2019-06-11 10:53:24 -0400

    Changed answer checking to multi feedback per question and added a button to move to next page if all correct

--- complete/src/main/resources/static/js/AnswerCheckControl.js
+++ complete/src/main/resources/static/js/AnswerCheckControl.js
@@ -24,6 +24,47 @@ function generateFeedbackMulti(isCheckedQ1, isCheckedQ2, isCheckedQ3) {
     }
 }
 
+function generateFeedbackPerQuestion(isCheckedQ1, isCheckedQ2, isCheckedQ3) {
+    var outputString = "";
+    if (isCheckedQ1 && isCheckedQ2 && isCheckedQ3) {
+        outputString = "All three correct!";
+        return outputString;
+    } else if (isCheckedQ1 && isCheckedQ2) {
+        outputString = "Question 3 is wrong";
+        return outputString;
+    } else if (isCheckedQ1 && isCheckedQ3) {
+        outputString = "Question 2 is wrong";
+        return outputString;
+    } else if (isCheckedQ2 && isCheckedQ3) {
+        outputString = "Question 1 is wrong";
+        return outputString;
+    } else if (isCheckedQ1) {
+        outputString = "Questions 2 and 3 are wrong";
+        return outputString;
+    } else if (isCheckedQ2) {
+        outputString = "Questions 1 and 3 are wrong";
+        return outputString;
+    } else if (isCheckedQ3) {
+        outputString = "Questions 1 and 2 are wrong";
+        return outputString;
+    } else {
+        outputString = "All three are wrong.";
+        return outputString;
+    }
+
+}
+
 function displayFeedback(displayString) {
     document.getElementById('answer').innerHTML = displayString;
+    displayToggableButton();
+}
+
+function displayToggableButton() {
+    var toggableStateButtonDeterminer = document.getElementById("answer").innerHTML;
+    var toggableStateButton = document.getElementById("toggableButton");
+    if (toggableStateButtonDeterminer == "All three correct!") {
+        toggableStateButton.style.display = 'block';
+    } else {
+        toggableStateButton.style.display = 'none';
+    }
 }
