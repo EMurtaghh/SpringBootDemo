a3887f4257feefbaf2fa60b29aa120058fea0cbd
Aine <m.g.firefox@gmail.com>
2019-06-12 10:54:51 -0400

    small tweaks, made ImageTaskResponse class

---
+++ complete/src/main/java/hello/ImageTaskResponse.java
@@ -0,0 +1,8 @@
+package hello;
+
+import java.util.List;
+
+public class ImageTaskResponse {
+    List<String> taskQuestionIds;
+    List<String> responseTexts;
+}
--- complete/src/main/resources/static/js/Change.js
+++ complete/src/main/resources/static/js/Change.js
@@ -18,9 +18,9 @@ function createMulti(){
     };
     
     return "<p>"+json.questionText+'</p> <input type="radio" th:field="*{plane}" value="Incorrect">'+json.answers[0]
-        +'<br> <input type="radio" th:field="*{plane}" value="Correct">'+json.answers[1]
-        +'<br> <input type="radio" th:field="*{plane}" value="Incorrect">'+json.answers[2]
-        +'<br> <input type="radio" th:field="*{plane}" value="Incorrect">'+json.answers[3]+'<br>';
+        +'<br> <input type="radio" name="plane" value="Correct">'+json.answers[1]
+        +'<br> <input type="radio" name="plane" value="Incorrect">'+json.answers[2]
+        +'<br> <input type="radio" name="plane" value="Incorrect">'+json.answers[3]+'<br>';
 }
 
 function createFillIn(){
@@ -47,11 +47,6 @@ function change() {
     }
 }
 
-// function getImageTask(){
-//     var task = [[${currentTask}]];
-// }
-
-
 function changeQuestions() {
     document.getElementById('image').innerHTML = remove();
     change();
@@ -69,12 +64,12 @@ function changeQuestions() {
     document.getElementById('zoneCorrect').innerHTML = remove();
 }
 
-function addResponesToRecords(resourceRecords) {
+function addResponesToRecords() {
     var newResponse = {
 
     };
 
-    resourceRecords.push(newResponse);
+    //resourceRecords.push(newResponse);
 }
 
 function displayCheck(value){
