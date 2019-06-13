b9d9444cba580b68d0bf62c3a3c91ee974463c56
Aine <m.g.firefox@gmail.com>
2019-06-13 13:00:37 -0400

    controller name change still

--- complete/src/main/java/hello/RestingController.java
+++ complete/src/main/java/hello/RestingController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class RestController {
+public class RestingController {
 
     ParServer parServer;
 
-    RestController(){
+    RestingController(){
         super();
         parServer = new ParServer();
     }
